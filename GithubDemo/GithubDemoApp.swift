df84415a2de79a6c2af59f013e87b9a96a83e153
Smith Dabre <dabresmith97@gmail.com>
2023-01-10 09:58:45 -0500

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by user221684 on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
